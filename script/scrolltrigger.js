1cececf1dff6d804035374e5893eddc938d9673d
popenjo <popenjo>
2023-09-21 00:08:25 +0900

    포트폴리오 수정 2차

---
+++ script/scrolltrigger.js
@@ -0,0 +1,13 @@
+gsap.to('.left',{
+    scrollTrigger:'#wrap',
+    duration:1.5,
+    x: 0,
+    opacity:1,
+})
+
+gsap.to('.right',{
+    scrollTrigger:'#wrap',
+    duration:1.5,
+    x: 0,
+    opacity:1,
+})
