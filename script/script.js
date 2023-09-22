01ea81f685b9302b58d86fcefeb25ccb60db7541
eojin <aejin0501@naver.com>
2023-09-22 16:19:41 +0900

    포트폴리오 수정본

---
+++ script/script.js
@@ -0,0 +1,54 @@
+$('.sns1_page').hide() //sns 첫번째 페이지 안보이게 하기
+$('.sns2_page').hide() //sns 두번째 페이지 안보이게 하기
+$('.sns3_page').hide() //sns 세번째 페이지 안보이게 하기
+$('.sns4_page').hide() //sns 네번째 페이지 안보이게 하기
+$('.sns5_page').hide() //sns 다섯번째 페이지 안보이게 하기
+$('.sns6_page').hide() //sns 여섯번째 페이지 안보이게 하기
+
+$('.sns_design > li:nth-child(1) > img').on('click',function(){
+    $('.sns1_page').show()
+})
+
+$('#closer').on('click',function(){
+    $('.sns1_page').hide()
+})
+// -------------------------------------------------
+$('.sns_design > li:nth-child(2) > img').on('click',function(){
+    $('.sns2_page').show()
+})
+
+$('#closer2').on('click',function(){
+    $('.sns2_page').hide()
+})
+// -------------------------------------------------
+$('.sns_design > li:nth-child(3) > img').on('click',function(){
+    $('.sns3_page').show()
+})
+
+$('#closer3').on('click',function(){
+    $('.sns3_page').hide()
+})
+// -------------------------------------------------
+$('.sns_design > li:nth-child(4) > img').on('click',function(){
+    $('.sns4_page').show()
+})
+
+$('#closer4').on('click',function(){
+    $('.sns4_page').hide()
+})
+// -------------------------------------------------
+$('.sns_design > li:nth-child(5) > img').on('click',function(){
+    $('.sns5_page').show()
+})
+
+$('#closer5').on('click',function(){
+    $('.sns5_page').hide()
+})
+// -------------------------------------------------
+$('.sns_design > li:nth-child(6) > img').on('click',function(){
+    $('.sns6_page').show()
+})
+
+$('#closer6').on('click',function(){
+    $('.sns6_page').hide()
+})
--- script/scrolltrigger.js
+++ script/scrolltrigger.js
@@ -1,11 +1,4 @@
-gsap.to('.left',{
-    scrollTrigger:'#wrap',
-    duration:1.5,
-    x: 0,
-    opacity:1,
-})
-
-gsap.to('.right',{
+gsap.to('.left, .right',{
     scrollTrigger:'#wrap',
     duration:1.5,
     x: 0,
